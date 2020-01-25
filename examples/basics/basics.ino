c931f06b3967e37dfa36076f65fe3d439102abd7
Jihoon Kim (Hanul) <kimwlgns4270@naver.com>
2020-01-26 01:08:14 +0900

    add comment
    consideration for beginners

--- examples/basics/basics.ino
+++ examples/basics/basics.ino
@@ -3,8 +3,11 @@
  * UltraSonic Distance Sensor Library
  */
 
+//including UltraSonic header File
 #include <UltraSonic.h>
 
+//make UltraSonic Object
+//UltraSonic [Object Name]([Trig], [Echo]);
 UltraSonic us(7, 6);
 
 void setup() {
@@ -13,18 +16,28 @@ void setup() {
 
 void loop() {
 
+  //distPrint()
+  //Serial printing distance Value ("distance : [value] cm\n")
   us.distPrint();
   delay(1000);
 
+  //distPrint(int ms)
+  //Serial printing distance Value ("distance : [value] cm\n")
+  //+ delay(ms)
   us.distPrint(1000);
 
+  //distByInt()
+  //sensing distance and return distance value(int)
   int distInt = us.distByInt();
   
-
+  //+ delay(ms)
   int distInt = us.distByInt(1000);
 
+  //distByFloat()
+  //sensing distance and return distance value(float)
   float distFloat = us.distByFloat();
 
+  //+ delay(ms)
   float distFloat = us.distByFloat(1000);
 
 }
