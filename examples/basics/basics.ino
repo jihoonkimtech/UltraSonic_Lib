0d710929d6a3a6752363b2a9cc6dd7bbf2e15068
Jihoon Kim (Hanul) <kimwlgns4270@naver.com>
2020-01-31 01:47:50 +0900

    Add Functional Description
    PIN_TYPE, LENGTH_TYPE

--- examples/basics/basics.ino
+++ examples/basics/basics.ino
@@ -7,8 +7,9 @@
 #include <UltraSonic.h>
 
 //make UltraSonic Object
-//UltraSonic [Object Name]([Trig], [Echo]);
-UltraSonic us(7, 6);
+//UltraSonic [Object Name]([PIN_TYPE],[Trig], [Echo]);
+//PIN_TYPE : THREE_PIN(sig) , FOUR_PIN(echo.gnd)
+UltraSonic us(FOUR_PIN, 7, 6);
 
 void setup() {
   Serial.begin(9600);
@@ -21,10 +22,11 @@ void loop() {
   us.distPrint();
   delay(1000);
 
-  //distPrint(int ms)
-  //Serial printing distance Value ("distance : [value] cm\n")
-  //+ delay(ms)
-  us.distPrint(1000);
+  //distPrint(LENGTH_TYPE,int ms)
+  //Serial printing distance Value ("distance : [value] [LENGTH_TYPE]\n")
+  //LENGTH_TYPE FEET(ft), CM(cm), MM(mm), INCH(in), METER(m). //default = CM
+  //+ delay(ms) //default = 0
+  us.distPrint(CM, 1000);
 
   //ensureDist()
   //sensing distance and return distance value(default float type)
