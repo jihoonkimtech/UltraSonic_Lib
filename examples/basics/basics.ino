34360e2adb179f8b4d34cc0b32d9a7d09130e2e0
Jihoon Kim (Hanul) <kimwlgns4270@naver.com>
2020-03-24 02:04:33 +0900

    fix: Edit for Version 1.0.1

--- examples/basics/basics.ino
+++ examples/basics/basics.ino
@@ -1,6 +1,8 @@
 /*
  * UltraSonic.h
  * UltraSonic Distance Sensor Library
+ * 
+ * for 1.0.1
  */
 
 //including UltraSonic header File
@@ -9,7 +11,7 @@
 //make UltraSonic Object
 //UltraSonic [Object Name]([PIN_TYPE],[Trig], [Echo]);
 //PIN_TYPE : THREE_PIN(sig) , FOUR_PIN(echo.gnd)
-UltraSonic us(FOUR_PIN, 7, 6);
+UltraSonic us_sensor(FOUR_PIN, 7, 6);
 
 void setup() {
   Serial.begin(9600);
@@ -19,26 +21,26 @@ void loop() {
 
   //distPrint()
   //Serial printing distance Value ("distance : [value] cm\n")
-  us.distPrint();
+  us_sensor.distPrint();
   delay(1000);
 
-  //distPrint(LENGTH_TYPE,int ms)
+  //distPrint(int ms, int LENGTH_TYPE)
   //Serial printing distance Value ("distance : [value] [LENGTH_TYPE]\n")
   //LENGTH_TYPE FEET(ft), CM(cm), MM(mm), INCH(in), METER(m). //default = CM
   //+ delay(ms) //default = 0
-  us.distPrint(CM, 1000);
+  us_sensor.distPrint(1000, CM);
 
   //ensureDist()
   //sensing distance and return distance value(default float type)
-  int distInt = us.distByInt(); //auto-casting
+  int distInt = us_sensor.distByInt(); //auto-casting
   
   //+ delay(ms)
-  int distInt = us.distByInt(1000); //auto-casting
+  int distInt = us_sensor.distByInt(1000); //auto-casting
 
 
-  float distFloat = us.distByFloat(); //normal use
+  float distFloat = us_sensor.distByFloat(); //normal use
 
   //+ delay(ms)
-  float distFloat = us.distByFloat(1000);
+  float distFloat = us_sensor.distByFloat(1000);
 
 }
